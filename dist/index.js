599c288163e96443eaf12cd83f485b211fc6b50a
Narumi <narumi@Narumis-Macbook-Pro.local>
2020-03-30 10:16:12 +0200

    Testing Input-looks

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,10 @@ var router = require('./router/router.js');
 var dotenv = require('dotenv');
 var bodyParser = require('body-parser');
 var app = express();
+var jquery = require('jquery');
+
+//Static file location
+app.use(express.static(__dirname + '/public'));
 
 dotenv.config({ path: './config/.env' });
 
@@ -14,6 +18,25 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(router);
 
+/// Animation 
+
+// // jquery transit is used to handle the animation
+// $('input').focusin(function() {
+//   $('label').transition({x:'80px'},500,'ease').next()
+//   .transition({x:'5px'},500, 'ease');
+// //setTimeout needed for Chrome, for some reson there is no animation from left to right, the pen is immediately present. Slight delay to adding the animation class fixes it
+//    setTimeout(function(){
+//   $('label').next().addClass('move-pen');
+//   },100);
+
+// });
+
+// $('input').focusout(function() {
+//     $('label').transition({x:'0px'},500,'ease').next()
+//      .transition({x:'-100px'},500, 'ease').removeClass('move-pen');
+// });
+
+/// Animation end
 var port = process.env.PORT || 3100;
 var options = {
   useUnifiedTopology: true,
@@ -22,4 +45,5 @@ var options = {
 mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0-1lxdq.mongodb.net/test', options).then(function () {
   console.log("Successful");
   app.listen(port);
+  //test
 });
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const router = require('./router/router.js');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser');
 const app = express();
+const jquery = require('jquery');
+
+
+//Static file location
+app.use(express.static(__dirname + '/public'));
 
 dotenv.config({ path: './config/.env' });
 
@@ -12,7 +17,25 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(router)
 
+/// Animation 
 
+// // jquery transit is used to handle the animation
+// $('input').focusin(function() {
+//   $('label').transition({x:'80px'},500,'ease').next()
+//   .transition({x:'5px'},500, 'ease');
+// //setTimeout needed for Chrome, for some reson there is no animation from left to right, the pen is immediately present. Slight delay to adding the animation class fixes it
+//    setTimeout(function(){
+//   $('label').next().addClass('move-pen');
+//   },100);
+
+// });
+
+// $('input').focusout(function() {
+//     $('label').transition({x:'0px'},500,'ease').next()
+//      .transition({x:'-100px'},500, 'ease').removeClass('move-pen');
+// });
+
+/// Animation end
 const port = process.env.PORT || 3100;
 const options ={
       useUnifiedTopology: true, 
