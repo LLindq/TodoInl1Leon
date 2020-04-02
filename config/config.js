514f6aa0b96210708ccda6758737500ad81a13e7
Narumi <narumi@Narumis-Macbook-Pro.local>
2020-04-02 13:46:08 +0200

    Heroku test

--- config/config.js
+++ config/config.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 
 const config = {
-    databaseURL: process.env.DATABASE,
-    DB_PASS: process.env.DB_PASS,
-    DB_USER: process.env.DB_USER
-    
+    databaseURL: process.env.DATABASE    
 };
 
 module.exports = config;
